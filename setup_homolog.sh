13357ed8d447e1aeede55eccefdc012f6d1210e9
ViniMPostal <viniciusmppostal@gmail.com>
2026-06-22 23:36:57 -0300

    Corrige scripts de criacao dos ambientes

--- setup_homolog.sh
+++ setup_homolog.sh
@@ -4,7 +4,7 @@ echo "Criando ambiente de Homologação..."
 
 docker rm -f homolog-app homolog-db 2>/dev/null || true
 
-docker-compose -f docker-compose.homolog.yml -p homolog up -d --build
+sudo docker-compose -f docker-compose.homolog.yml -p homolog up -d --build
 
 echo "Homologação disponível em:"
 echo "http://177.44.248.92:8080/login.php"
--- setup_producao.sh
+++ setup_producao.sh
@@ -4,7 +4,7 @@ echo "Criando ambiente de Produção..."
 
 docker rm -f producao-app producao-db 2>/dev/null || true
 
-docker-compose -f docker-compose.producao.yml -p producao up -d --build
+sudo docker-compose -f docker-compose.producao.yml -p producao up -d --build
 
 echo "Produção disponível em:"
 echo "http://177.44.248.92:8081/login.php"
