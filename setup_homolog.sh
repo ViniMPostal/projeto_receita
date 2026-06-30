fd9d0cd24ff5dc948463be45f5bc1049af6f13a0
ViniMPostal <viniciusmppostal@gmail.com>
2026-06-30 19:59:23 -0300

    Corrige scripts para subir app e banco

--- setup_homolog.sh
+++ setup_homolog.sh
@@ -3,8 +3,9 @@
 echo "Criando ambiente de Homologação..."
 
 sudo docker ps -a --format '{{.Names}}' | grep 'homolog-app' | xargs -r sudo docker rm -f
+sudo docker ps -a --format '{{.Names}}' | grep 'homolog-db' | xargs -r sudo docker rm -f
 
-sudo docker-compose -f docker-compose.homolog.yml -p homolog up -d --no-deps --build app
+sudo docker-compose -f docker-compose.homolog.yml -p homolog up -d --build
 
 echo "Homologação disponível em:"
 echo "http://177.44.248.92:8080/login.php"
--- setup_producao.sh
+++ setup_producao.sh
@@ -3,8 +3,9 @@
 echo "Criando ambiente de Produção..."
 
 sudo docker ps -a --format '{{.Names}}' | grep 'producao-app' | xargs -r sudo docker rm -f
+sudo docker ps -a --format '{{.Names}}' | grep 'producao-db' | xargs -r sudo docker rm -f 
 
-sudo docker-compose -f docker-compose.producao.yml -p producao up -d --no-deps --build app
+sudo docker-compose -f docker-compose.producao.yml -p producao up -d --build
 
 echo "Produção disponível em:"
 echo "http://177.44.248.92:8081/login.php"
